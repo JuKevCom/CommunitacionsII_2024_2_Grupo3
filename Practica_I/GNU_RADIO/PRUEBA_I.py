61a935b886b3d9719be8409440613ae5ec82b428
Comu2_GrupoC1 <comu2_grupoc1@labcom-Profe>
2024-08-22 11:21:09 -0500

    Ya terminamos todos los bloques

--- Practica_I/GNU_RADIO/PRUEBA_I.py
+++ Practica_I/GNU_RADIO/PRUEBA_I.py
@@ -32,8 +32,7 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import PRUEBA_I_epy_block_0 as epy_block_0  # embedded python block
-import PRUEBA_I_epy_block_1 as epy_block_1  # embedded python block
+import PRUEBA_I_epy_block_0_0 as epy_block_0_0  # embedded python block
 
 
 
@@ -80,32 +79,32 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            3, #number of inputs
+            1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_1.set_update_time(0.10)
+        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_1.enable_tags(True)
+        self.qtgui_time_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1.enable_autoscale(True)
+        self.qtgui_time_sink_x_1.enable_grid(False)
+        self.qtgui_time_sink_x_1.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_stem_plot(False)
 
 
-        labels = ['Acumulador', 'Diferenciador', 'Entrada', 'Signal 4', 'Signal 5',
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
-        colors = ['yellow', 'red', 'green', 'black', 'cyan',
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
@@ -115,69 +114,67 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(3):
+        for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_1.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_number_sink_0 = qtgui.number_sink(
+        self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
+        self.epy_block_0_0 = epy_block_0_0.blk()
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 1000, 0.5, 0, 0)
+        self.Medidas = qtgui.number_sink(
             gr.sizeof_float,
             0,
             qtgui.NUM_GRAPH_HORIZ,
-            3,
+            5,
             None # parent
         )
-        self.qtgui_number_sink_0.set_update_time(0.10)
-        self.qtgui_number_sink_0.set_title("")
+        self.Medidas.set_update_time(0.10)
+        self.Medidas.set_title("")
 
-        labels = ["Acumulador", "Diferenciador", "Entrada", '', '',
+        labels = ['Media', 'Media cuadràtica', 'RMS', 'Potencia Promedio', 'Desviaciòn Estàndar',
             '', '', '', '', '']
         units = ['', '', '', '', '',
             '', '', '', '', '']
-        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+        colors = [("black", "black"), ("blue", "red"), ("black", "black"), ("black", "black"), ("black", "black"),
             ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
         factor = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
 
-        for i in range(3):
-            self.qtgui_number_sink_0.set_min(i, -10000)
-            self.qtgui_number_sink_0.set_max(i, 10000)
-            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+        for i in range(5):
+            self.Medidas.set_min(i, -10000)
+            self.Medidas.set_max(i, 10000)
+            self.Medidas.set_color(i, colors[i][0], colors[i][1])
             if len(labels[i]) == 0:
-                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+                self.Medidas.set_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_number_sink_0.set_label(i, labels[i])
-            self.qtgui_number_sink_0.set_unit(i, units[i])
-            self.qtgui_number_sink_0.set_factor(i, factor[i])
+                self.Medidas.set_label(i, labels[i])
+            self.Medidas.set_unit(i, units[i])
+            self.Medidas.set_factor(i, factor[i])
 
-        self.qtgui_number_sink_0.enable_autoscale(False)
-        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
-        self.qtgui_number_sink_0.set_block_alias("Acumulador")
-        self.epy_block_1 = epy_block_1.blk()
-        self.epy_block_0 = epy_block_0.blk()
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 10, 0.5, 0, 0)
+        self.Medidas.enable_autoscale(False)
+        self._Medidas_win = sip.wrapinstance(self.Medidas.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._Medidas_win)
+        self.Medidas.set_block_alias("Acumulador")
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_1, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_number_sink_0, 2))
-        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 2))
-        self.connect((self.epy_block_0, 0), (self.qtgui_number_sink_0, 0))
-        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.epy_block_1, 0), (self.qtgui_number_sink_0, 1))
-        self.connect((self.epy_block_1, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_0_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_1, 0))
+        self.connect((self.epy_block_0_0, 0), (self.Medidas, 0))
+        self.connect((self.epy_block_0_0, 3), (self.Medidas, 3))
+        self.connect((self.epy_block_0_0, 2), (self.Medidas, 2))
+        self.connect((self.epy_block_0_0, 1), (self.Medidas, 1))
+        self.connect((self.epy_block_0_0, 4), (self.Medidas, 4))
 
 
     def closeEvent(self, event):
@@ -194,7 +191,7 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
 
 
 
