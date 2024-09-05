086d2230d2466df90cf237408d415873cee7e0dc
Comu2_GrupoC1 <comu2_grupoc1@labcomu10>
2024-09-05 12:10:30 -0500

    Completado

--- Practica_I/GNU_RADIO/PRUEBA_I.py
+++ Practica_I/GNU_RADIO/PRUEBA_I.py
@@ -6,38 +6,28 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# GNU Radio version: 3.9.8.0
-
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: v3.11.0.0git-810-g1ecb8565
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import PRUEBA_I_epy_block_0 as epy_block_0  # embedded python block
+import PRUEBA_I_epy_block_0_0_0 as epy_block_0_0_0  # embedded python block
 import PRUEBA_I_epy_block_1_0 as epy_block_1_0  # embedded python block
+import sip
+import threading
 
 
-
-from gnuradio import qtgui
 
 class PRUEBA_I(gr.top_block, Qt.QWidget):
 
@@ -48,8 +38,8 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -62,15 +52,15 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "PRUEBA_I")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "PRUEBA_I")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
@@ -80,10 +70,59 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
+
+        self.qtgui_time_sink_x_1_1 = qtgui.time_sink_f(
+            100000, #size
+            samp_rate, #samp_rate
+            "INTEGRAL", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_1_1.set_update_time(0.5)
+        self.qtgui_time_sink_x_1_1.set_y_axis(0, 1)
+
+        self.qtgui_time_sink_x_1_1.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_1_1.enable_tags(False)
+        self.qtgui_time_sink_x_1_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1_1.enable_autoscale(True)
+        self.qtgui_time_sink_x_1_1.enable_grid(False)
+        self.qtgui_time_sink_x_1_1.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1_1.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_1_1.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_1_1.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1_1.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1_1.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1_1.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1_1.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1_1.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_1_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_1_win)
         self.qtgui_time_sink_x_1_0 = qtgui.time_sink_f(
             100, #size
             samp_rate, #samp_rate
-            "", #name
+            "SEÑAL ORIGINAL", #name
             2, #number of inputs
             None # parent
         )
@@ -131,7 +170,7 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
             100000, #size
             samp_rate, #samp_rate
-            "", #name
+            "DERIVADA", #name
             1, #number of inputs
             None # parent
         )
@@ -177,9 +216,45 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
         self.epy_block_1_0 = epy_block_1_0.blk()
+        self.epy_block_0_0_0 = epy_block_0_0_0.blk()
+        self.epy_block_0 = epy_block_0.blk()
         self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_TRI_WAVE, 3200, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(60000, analog.GR_COS_WAVE, 3200, 1, 0, 0)
         self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 0.1, 0)
+        self.Medidas_1 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            5,
+            None # parent
+        )
+        self.Medidas_1.set_update_time(0.10)
+        self.Medidas_1.set_title("1")
+
+        labels = ['Mediar', 'Media cuadràticar', 'RMSr', 'Potencia Promedior', 'Desviaciòn Estàndarr',
+            '', '', '', '', '']
+        units = ['', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("black", "black"), ("blue", "red"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(5):
+            self.Medidas_1.set_min(i, -10000)
+            self.Medidas_1.set_max(i, 10000)
+            self.Medidas_1.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.Medidas_1.set_label(i, "Data {0}".format(i))
+            else:
+                self.Medidas_1.set_label(i, labels[i])
+            self.Medidas_1.set_unit(i, units[i])
+            self.Medidas_1.set_factor(i, factor[i])
+
+        self.Medidas_1.enable_autoscale(False)
+        self._Medidas_1_win = sip.wrapinstance(self.Medidas_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._Medidas_1_win)
+        self.Medidas_1.set_block_alias("Acumulador")
 
 
         ##################################################
@@ -190,11 +265,19 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
         self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_1_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.epy_block_1_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_1_0, 1))
+        self.connect((self.epy_block_0, 0), (self.epy_block_0_0_0, 0))
+        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_1_1, 0))
+        self.connect((self.epy_block_0_0_0, 1), (self.Medidas_1, 1))
+        self.connect((self.epy_block_0_0_0, 3), (self.Medidas_1, 3))
+        self.connect((self.epy_block_0_0_0, 4), (self.Medidas_1, 4))
+        self.connect((self.epy_block_0_0_0, 0), (self.Medidas_1, 0))
+        self.connect((self.epy_block_0_0_0, 2), (self.Medidas_1, 2))
+        self.connect((self.epy_block_1_0, 0), (self.epy_block_0, 0))
         self.connect((self.epy_block_1_0, 0), (self.qtgui_time_sink_x_1, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "PRUEBA_I")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "PRUEBA_I")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -206,23 +289,21 @@ class PRUEBA_I(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
         self.qtgui_time_sink_x_1_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1_1.set_samp_rate(self.samp_rate)
 
 
 
 
 def main(top_block_cls=PRUEBA_I, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
 
     tb.start()
+    tb.flowgraph_started.set()
 
     tb.show()
 
